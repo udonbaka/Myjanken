bb9776445fdb1c9430de95b073da98dab8647f77
udonbaka <udonbaka@gmail.com>
2022-06-03 10:24:08 +0900

    Initial Commit

---
+++ Myjanken/MyjankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyjankenApp.swift
+//  Myjanken
+//
+//  Created by Hide Shin on 2022/06/03.
+//
+
+import SwiftUI
+
+@main
+struct MyjankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
